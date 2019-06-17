1800c2b363a58c950f05c642ddde5de7b140e3f7
MiloTranseunte <50523240+MiloTranseunte@users.noreply.github.com>
2019-06-16 21:22:10 -0300

    Launcher Implemented
    Launcher already on scene, almost fully functional. Some issues come around like not measure the distance between the body and the distance to the ground.

--- Entities/Enemies/Launcher/Launcher.gd
+++ Entities/Enemies/Launcher/Launcher.gd
@@ -9,21 +9,24 @@ export (float) var angle = 45
 export (float) var shoot_force = 50
 
 export (float) var gravity = 100
-export (float) var velocity = 20
+export (float) var velocity = 40
 export (Vector2) var motion = Vector2()
 
 # Survival variables
 var is_dead = false
+var attacking = false
 
 # Shooting Variables
 var can_shoot = true
-const FORCE_BALL = preload("res://Powers/enemyPowers/Fireball/enemyFireball.tscn")
+const FORCE_BALL = preload("res://Powers/enemyPowers/force_ball/force_ball.tscn")
 
 var facing = 1
 
+var animation_playing
+
 func takeDamage(hitPoint):
 	if is_dead == false:
-		var health = $health._damage(hitPoint)
+		var health = $launcher_health._damage(hitPoint)
 		if health <= 0:
 			_dead()
 
@@ -31,43 +34,61 @@ func takeDamage(hitPoint):
 func _dead():
 	is_dead = true
 	motion = Vector2(0,0) 
-	$Sprite.play("idle")
-	$entityCollision.disabled = true
+	$animation.play("dead")
+	yield($animation, "animation_finished")
 	$areaDetection/detectionShape.disabled = true
-	# $Timer.start()
+	$entityCollision.disabled = true
+	queue_free()
 	pass
+	
+func shootTrigered():
+	attacking = true
+	
+	if is_dead == false && can_shoot == true:
+		can_shoot = false
+		
+		$animation.play("attack")
+		yield($animation, "animation_finished")
+		
+		if $animation.get_animation() != "dead":
+			var _shoot_direction = -1 # negative left
+			var force_ball_pos = $pos_left_cannon.global_position
+			
+			var force_ball = FORCE_BALL.instance()
+			get_parent().add_child(force_ball)
+			force_ball.position = force_ball_pos
+			force_ball.launch(_shoot_direction)
+			
+			force_ball.beforeVanish()
+			
+			_shoot_direction *= -1 # EQUALS TO +1 positive right
+			force_ball_pos = $pos_right_cannon.global_position
+			
+			force_ball = FORCE_BALL.instance()
+			get_parent().add_child(force_ball)
+			force_ball.position = force_ball_pos
+			force_ball.launch(_shoot_direction)
+			
+			force_ball.beforeVanish()
+			
+		can_shoot = true
+		attacking = false
 	
 func _physics_process(delta):
-	motion.x = velocity * facing
-
-	var force_ball_pos = 0 # $pos_left_cannon.global_position
-
-	var force_ball = FORCE_BALL
 	
-	if Input.is_action_just_pressed("throw"): # SPACEBAR
+	motion.y += gravity - gravity
 	
-		var _shoot_direction = -1 # negative left
-		force_ball_pos = $pos_left_cannon.global_position
-		force_ball = FORCE_BALL.instance()
-		get_parent().add_child(force_ball)
-		force_ball.position = force_ball_pos
-		force_ball.launch(_shoot_direction)
+	print($distanceGround.get_collision_point())
+	
+	$Label.text = str($launcher_health.get_health())
+	
+	if is_dead == false:
+		if attacking == false:
+			$animation.play("idle")
 		
-		force_ball.beforeVanish()
+		motion.x = velocity * facing
 		
-		_shoot_direction *= -1 # EQUALS TO +1 positive right
-		force_ball_pos = $pos_right_cannon.global_position
-		force_ball = FORCE_BALL.instance()
-		get_parent().add_child(force_ball)
-		force_ball.position = force_ball_pos
-		force_ball.launch(_shoot_direction)
-		
-		force_ball.beforeVanish()
-		
-	motion = move_and_slide(motion, Vector2(0, -1))
+		motion = move_and_slide(motion, UP)
 		
 	if is_on_wall():
-		print("wall")
 		facing *= -1
-	
-	pass
